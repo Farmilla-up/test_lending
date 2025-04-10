32bc3aa7a4bc9036c1fec1fef82dfe3ce6933c9f
BoberKurwa <artemiyst1@gmail.com>
2025-04-10 22:46:42 +0200

    Финалочка 2.0

--- static/js/form.js
+++ static/js/form.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const successMessage = document.querySelector('#contact-form-section p');
 
-    // Если сообщение об успехе или ошибке есть — прокрутим к форме
+
     if (successMessage) {
         location.hash = 'contact-form';
     }
